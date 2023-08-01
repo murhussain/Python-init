ca78ee2c922a9af82ccb8d5b05bfadc2ee4663b8
Murhussain <husseinmurashi@yahoo.com>
2023-08-01 17:10:45 +0300

    Working on first challenge from book

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,8 +1,9 @@
 fName = "Hussein"
 lName = "MURASHI"
-
 FullName = f"{fName} {lName}"
 
 print("Capitalized name: " + FullName.title())
 print("Lowercase name: " + FullName.lower())
 print("Uppercase name: " + FullName.upper())
+print("Uppercase name: " + FullName.upper())
+
