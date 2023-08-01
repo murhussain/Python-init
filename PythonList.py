f028717e86c79d0a97114014bfe537683b664b0c
Murhussain <husseinmurashi@yahoo.com>
2023-08-01 17:38:10 +0300

    Appending, and inserting in a list using Python

---
+++ PythonList.py
@@ -0,0 +1,9 @@
+stringList = ['Hello', 'Python', 'World']
+
+print(stringList)
+print(f'Accessing individual element from  list: ----{stringList[2]}')
+stringList.append('Mur')
+print(f"Appended list: ------{stringList}")
+
+stringList.insert(1, 'Hussein')
+print(f"Inserted object in a list: ------{stringList}")
